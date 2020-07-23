17b0bef5cfed57088e26fef50863f2429087c03e
MIDET-tech <63294970+MIDET-tech@users.noreply.github.com>
2020-07-23 17:43:35 +0900

    Initial commit

---
+++ PDF_combiner.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import PySimpleGUI as sg
+import PyPDF2
+import os
+import warnings
+
+def submit_func():
+    merger = PyPDF2.PdfFileMerger()
+    for i in range(number):
+        merger.append(values[i])
+    merger.write("combined_file.pdf".format(user_name))
+    merger.close()
+    
+    sg.popup("The combined PDF file has been output successfully as [combined_file.pdf]")
+
+warnings.simplefilter("ignore")
+user_name = os.getlogin()
+
+#Number of files
+number = 2
+
+sg.theme("SandyBeach")
+
+layout = [[sg.Text("Browse 2 or more pdf files you want to combine")],
+          [sg.Text("File 1", size=(8, 1)),sg.Input(), sg.FileBrowse()],
+          [sg.Text("File 2", size=(8, 1)),sg.Input(), sg.FileBrowse()],
+          [sg.Button("AddFile", key="AddFile"),sg.Submit(), sg.Cancel()]]
+
+window = sg.Window("PDF combiner", layout)
+
+while True:
+    event, values = window.read()
+
+    if event in [None,"Cancel"]:
+        break
+    elif event == "AddFile":
+        number += 1
+        new_layout = [[sg.Text("Browse 2 or more pdf files you want to combine")],
+              [sg.Text("File 1", size=(8, 1)),sg.Input(), sg.FileBrowse()],
+              [sg.Text("File 2", size=(8, 1)),sg.Input(), sg.FileBrowse()],
+              [sg.Button("AddFile", key="AddFile"),sg.Submit(), sg.Cancel()]]
+        for i in range(number-2):
+            additional_file = [sg.Text("File {}".format(i+3), size=(8, 1)),sg.Input(), sg.FileBrowse()]
+            new_layout.insert(i+3 , additional_file)
+        window.close()
+        window = sg.Window("PDF combiner", new_layout)
+    elif event == "Submit":
+        if values[0] and values[1] != None:
+            submit_func()
+            break
+        else:
+            sg.popup("Please browse more than 2 files")
+
+window.close()
+
